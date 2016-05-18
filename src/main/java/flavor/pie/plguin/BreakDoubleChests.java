44cc1a563472c79dbe305a4c9822cd374b2a3844
Adam Spofford <pieflavor.mc@gmail.com>
2016-05-17 20:38:57 -0700

    Use digBlock(), which hopefully is implemented

--- src/main/java/flavor/pie/plguin/BreakDoubleChests.java
+++ src/main/java/flavor/pie/plguin/BreakDoubleChests.java
@@ -1,23 +1,32 @@
 package flavor.pie.plguin;
 
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.Transaction;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.cause.entity.spawn.BlockSpawnCause;
 import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 public class BreakDoubleChests {
-    @Listener
+    Map<Location, Location> tracked = Maps.newHashMap();
+    @Inject
+    Plguin plguin;
+    @Listener(order= Order.BEFORE_POST)
     public void breakDoubleChests(ChangeBlockEvent.Break e, @First Player p) {
         List<Transaction<BlockSnapshot>> list = e.getTransactions();
         for (Transaction<BlockSnapshot> transaction : list) {
@@ -30,10 +39,27 @@ public class BreakDoubleChests {
                         Location<World> location = snapshot.getLocation().get();
                         for (Direction direction : set) {
                             Location<World> relative = location.getRelative(direction);
-                            relative.setBlockType(BlockTypes.AIR);
+                            /*tracked.put(location, relative);
+                            BlockSnapshot originalSnapshot = relative.createSnapshot();
+                            BlockState originalState = originalSnapshot.getState();
+                            BlockState proposedState = BlockState.builder().from(originalState).blockType(BlockTypes.AIR).build();
+                            BlockSnapshot proposedSnapshot = BlockSnapshot.builder().from(originalSnapshot).blockState(proposedState).build();
+                            ChangeBlockEvent.Break breakEvent = SpongeEventFactory.createChangeBlockEventBreak(e.getCause(), e.getTargetWorld(), Lists.newArrayList(new Transaction(originalSnapshot, proposedSnapshot)));
+                            */
+                            relative.getExtent().digBlock(relative.getBlockPosition(), e.getCause());
                         }
                     }
                 }
+            }
+        }
+    }
+    //@Listener
+    public void dropItems(DropItemEvent.Destruct e, @First BlockSpawnCause cause) {
+        Optional<Location<World>> loc_ = cause.getBlockSnapshot().getLocation();
+        if (loc_.isPresent()) {
+            Location<World> loc = loc_.get();
+            if (tracked.containsKey(loc)) {
+
             }
         }
     }
